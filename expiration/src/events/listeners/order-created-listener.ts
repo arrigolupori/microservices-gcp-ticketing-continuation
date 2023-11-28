6b8a8e5b66cf87f68b6fbd15037646513876d2ce
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-28 23:01:50 +0100

    initialized bull js job processing

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,18 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@sagewillcom/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
+}
